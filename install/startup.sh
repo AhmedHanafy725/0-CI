81e13930b25d3b0fffdad543ed390dd7a2136385
ahmedhanafy725 <hanafya@codescalers.com>
2020-04-26 13:19:23 +0200

    AOF redis

--- install/startup.sh
+++ install/startup.sh
@@ -3,7 +3,7 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa -q -P ""
 cp /root/.ssh/id_rsa.pub /sandbox/.ssh/authorized_keys
 source /sandbox/env.sh
 cd /sandbox/code/github/threefoldtech/zeroCI/backend
-redis-server &> redis.log &
+redis-server /etc/redis/redis.conf
 for i in {1..5}; do python3 worker.py &> worker_$i.log & done
 rqscheduler &> schedule.log &
 service nginx start
