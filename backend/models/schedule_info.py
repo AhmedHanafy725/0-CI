fda17b1073098aa924e8c3088d41941a06fa3939
ahmedhanafy725 <hanafya@codescalers.com>
2020-05-11 17:17:58 +0200

    Validate schedule script

--- backend/models/schedule_info.py
+++ backend/models/schedule_info.py
@@ -7,10 +7,11 @@ class ScheduleInfo(Base):
     _bcdb = j.data.bcdb.get("zeroci")
     _schema_text = """@url = zeroci.schedule.info
     name** = (S)
-    install_script = (S)
+    install = (S)
     run_time = (S)
-    test_script = (dict)
-    prerequisites = (dict)
+    script = (dict)
+    prerequisites = (S)
+    created_by = (S)
     """
     _schema = j.data.schema.get_from_text(_schema_text)
     _model = _bcdb.model_get(schema=_schema)
@@ -21,12 +22,12 @@ class ScheduleInfo(Base):
         else:
             self._model_obj = self._model.new()
             self._model_obj.name = kwargs["schedule_name"]
-            self._model_obj.install_script = kwargs["install_script"]
+            self._model_obj.install_script = kwargs["install"]
             self._model_obj.run_time = kwargs["run_time"]
-            self._model_obj.test_script = {"test_script": []}
-            self._model_obj.test_script["test_script"] = kwargs.get("test_script", [])
-            self._model_obj.prerequisites = {"prerequisites": []}
-            self._model_obj.prerequisites["prerequisites"] = kwargs.get("prerequisites", [])
+            self._model_obj.script = {"script": []}
+            self._model_obj.script["script"] = kwargs.get("script", [])
+            self._model_obj.prerequisites = kwargs["prerequisites"]
+            self._model_obj.created_by = kwargs["created_by"]
 
     @property
     def name(self):
@@ -37,12 +38,12 @@ class ScheduleInfo(Base):
         self._model_obj.name = name
 
     @property
-    def install_script(self):
-        return self._model_obj.install_script
+    def install(self):
+        return self._model_obj.install
 
-    @install_script.setter
-    def install_script(self, install_script):
-        self._model_obj.install_script = install_script
+    @install.setter
+    def install(self, install):
+        self._model_obj.install = install
 
     @property
     def run_time(self):
@@ -53,17 +54,25 @@ class ScheduleInfo(Base):
         self._model_obj.run_time = run_time
 
     @property
-    def test_script(self):
-        return self._model_obj.test_script["test_script"]
+    def script(self):
+        return self._model_obj.script["script"]
 
-    @test_script.setter
-    def test_script(self, test_script):
-        self._model_obj.test_script["test_script"] = test_script
+    @script.setter
+    def script(self, script):
+        self._model_obj.script["script"] = script
 
     @property
     def prerequisites(self):
-        return self._model_obj.prerequisites["prerequisites"]
+        return self._model_obj.prerequisites
 
     @prerequisites.setter
     def prerequisites(self, prerequisites):
-        self._model_obj.prerequisites["prerequisites"] = prerequisites
+        self._model_obj.prerequisites = prerequisites
+
+    @property
+    def created_by(self):
+        return self._model_obj.created_by
+
+    @created_by.setter
+    def created_by(self, created_by):
+        self._model_obj.created_by = created_by
