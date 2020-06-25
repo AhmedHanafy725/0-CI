f7f513cb3ac2dff96b7a8d9259274b9e234b3dee
ahmedhanafy725 <hanafya@codescalers.com>
2020-06-25 17:00:32 +0200

    Fix redis path

--- install/startup.sh
+++ install/startup.sh
@@ -2,7 +2,7 @@ set -ex
 cd /sandbox/code/github/threefoldtech/zeroCI/backend
 cp ../install/cron /var/spool/cron/crontabs/root
 cp ../install/nginx.conf /etc/nginx/sites-available/default
-cp ../installredis.conf /etc/redis/redis.conf
+cp ../install/redis.conf /etc/redis/redis.conf
 redis-server /etc/redis/redis.conf
 for i in {1..5}; do cp worker.py worker$i.py; python3 worker$i.py &> worker_$i.log & done
 rqscheduler &> schedule.log &
