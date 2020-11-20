8ae51c0ed2c4c84c768e4f43715e68d9a5a51d79
AhmedHanafy725 <41957921+AhmedHanafy725@users.noreply.github.com>
2020-11-20 21:17:27 +0200

    Update startup.sh

--- install/startup.sh
+++ install/startup.sh
@@ -10,5 +10,4 @@ redis-server /etc/redis/redis.conf
 for i in {1..5}; do cp worker.py worker$i.py; python3 worker$i.py &> worker_$i.log & done
 rqscheduler &> schedule.log &
 service cron start
-python3 zeroci.py &> zeroci.log &
-caddy reverse-proxy --from $DOMAIN --to 0.0.0.0:6010
+python3 zeroci.py
